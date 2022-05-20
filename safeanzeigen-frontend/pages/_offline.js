8b5dcab3ebaf114d962ac85ad6807ca488c1635d
Sascha Majewsky <saschamaj@protonmail.com>
2022-05-20 21:30:41 +0200

    Added offline pwa message

---
+++ safeanzeigen-frontend/pages/_offline.js
@@ -0,0 +1,8 @@
+const _offline = () => {
+  return (
+    <div>
+      Es sieht so aus, als wärst du nicht mit dem Internet verbunden. Sobald du
+      die Verbindung wiederherstellst geht es weiter!
+    </div>
+  );
+};
