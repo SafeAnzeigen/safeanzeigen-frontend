ddc82a46a4dbd099812f2c342d2b02de03e3cf4f
Sascha Majewsky <saschamaj@protonmail.com>
2022-05-20 23:49:36 +0200

    Added initial signin and reset pages

--- safeanzeigen-frontend/pages/_app.js
+++ safeanzeigen-frontend/pages/_app.js
@@ -1,7 +1,31 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import * as React from "react";
+import { useRouter } from "next/router";
+import NProgress from "nprogress";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+{
+  /* <html class="h-full bg-gray-50">
+      <body class="h-full"> */
 }
 
-export default MyApp
+function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  React.useEffect(() => {
+    const handleRouteStart = () => NProgress.start();
+    const handleRouteDone = () => NProgress.done();
+
+    router.events.on("routeChangeStart", handleRouteStart);
+    router.events.on("routeChangeComplete", handleRouteDone);
+    router.events.on("routeChangeError", handleRouteDone);
+
+    return () => {
+      router.events.off("routeChangeStart", handleRouteStart);
+      router.events.off("routeChangeComplete", handleRouteDone);
+      router.events.off("routeChangeError", handleRouteDone);
+    };
+  }, [router]);
+
+  return <Component {...pageProps} />;
+}
+
+export default MyApp;
