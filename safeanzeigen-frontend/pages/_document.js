8350266b85db63e21671417683f21b8eb897a88e
Sascha Majewsky <saschamaj@protonmail.com>
2022-06-05 13:52:16 +0200

    PWA offline image fallback

--- safeanzeigen-frontend/pages/_document.js
+++ safeanzeigen-frontend/pages/_document.js
@@ -1,21 +1,29 @@
 import Document from "next/document";
 import { Html, Head, Main, NextScript } from "next/document";
-class AppDocument extends Document {
+
+export default class extends Document {
+  static async getInitialProps(ctx) {
+    return await Document.getInitialProps(ctx);
+  }
+
   render() {
     return (
       <Html lang="de">
         <Head>
           <meta
             name="description"
-            content="Safeanzeigen is a modern classified ad platform for exchanging goods, services and digital assets with a special focus on usability and security."
+            content="Wir bringen deine Kleinanzeigen mit Sicherheit groß raus"
           />
+          <meta name="theme-color" content="#2f70e9" />
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
         </Head>
         <body>
-          <Main></Main>
-          <NextScript></NextScript>
+          <Main />
+          <NextScript />
         </body>
       </Html>
     );
   }
 }
-export default AppDocument;
--- safeanzeigen-frontend/pages/_offline.js
+++ safeanzeigen-frontend/pages/_offline.js
@@ -1,10 +1,45 @@
-const _offline = () => {
+import Head from "next/head";
+
+/* export default function Offline() {
   return (
     <div>
-      Es sieht so aus, als wärst du nicht mit dem Internet verbunden. Sobald du
-      die Verbindung wiederherstellst geht es weiter!
+      <Head>
+        <title>Safeanzeigen</title>
+      </Head>
+      <div className="text-red-500">
+        Es sieht so aus, als wärst du nicht mit dem Internet verbunden. Sobald
+        du die Verbindung wiederherstellst geht es weiter!
+      </div>
     </div>
   );
-};
+} */
 
-export default _offline;
+export default function Offline() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+      <Head>
+        <title>Safeanzeigen - Du bist Offline</title>
+        <meta
+          name="description"
+          content="Wir bringen deine Kleinanzeigen mit Sicherheit groß raus"
+        />
+        <meta name="theme-color" content="#2f70e9" />
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png" />
+      </Head>
+
+      <div className="flex flex-col items-center justify-center min-h-screen bg-gray-800">
+        <img
+          src="/static/images/offline-image.png"
+          className="mb-2 not-draggable"
+          alt="Offline Icon"
+          style={{ width: "256px", height: "256px" }}
+        />
+        <div className="mx-8 text-lg font-extrabold text-center text-orange-200">
+          Du bist offline. Bitte verbinde dich mit dem Internet.
+        </div>
+      </div>
+    </div>
+  );
+}
