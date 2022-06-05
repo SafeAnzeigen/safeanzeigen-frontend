861f8fee74bf3a1406881faf476ba8bb32f5df9a
Sascha Majewsky <saschamaj@protonmail.com>
2022-06-05 11:13:26 +0200

    Serving tinier image load

--- safeanzeigen-frontend/components/Startpage/TinyCategoryCard.js
+++ safeanzeigen-frontend/components/Startpage/TinyCategoryCard.js
@@ -1,6 +1,17 @@
 import Link from "next/link";
 
 export default function TinyCategoryCard({ categoryName, imageUrl }) {
+  const createTinyPath = (imageUrl) => {
+    if (imageUrl) {
+      return (
+        imageUrl.split("/upload")[0] +
+        "/upload" +
+        "/h_144,w_112,c_scale" +
+        imageUrl.split("/upload")[1]
+      );
+    }
+  };
+
   return (
     <Link
       href={{
@@ -19,7 +30,7 @@ export default function TinyCategoryCard({ categoryName, imageUrl }) {
         <div className={`relative w-28 h-36 overflow-x-hidden`}>
           <img
             className={`rounded-xl`}
-            src={imageUrl}
+            src={createTinyPath(imageUrl)}
             layout="fill"
             style={{ objectFit: "cover", height: "144px", width: "112px" }}
             alt="Vorabansicht der Kleinanzeige"
--- safeanzeigen-frontend/next.config.js
+++ safeanzeigen-frontend/next.config.js
@@ -9,6 +9,8 @@ module.exports = withPWA({
   },
   pwa: {
     dest: "public",
+    register: true,
+    skipWaiting: true,
   },
   async redirects() {
     return [
